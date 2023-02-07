444eab8e44fdf5bc371f6e15870affc4811011e0
tobi110289 <tobias.feltes@web.de>
2023-02-07 20:21:32 +0100

    refactor(resolvers): added error handling

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -11,89 +11,119 @@ const prisma = new PrismaClient();
 export default {
   Transaction: {
     category: async (parent: Transaction): Promise<Category | null> => {
-      return parent.category_id
-        ? await prisma.category.findUnique({
-            where: { id: parent.category_id },
-          })
-        : null;
+      try {
+        return parent.category_id
+          ? await prisma.category.findUnique({
+              where: { id: parent.category_id },
+            })
+          : null;
+      } catch (error) {
+        throw error;
+      }
     },
     account: async (parent: Transaction): Promise<Account | null> => {
-      return await prisma.account.findUnique({
-        where: { id: parent.account_id },
-      });
+      try {
+        return await prisma.account.findUnique({
+          where: { id: parent.account_id },
+        });
+      } catch (error) {
+        throw error;
+      }
     },
   },
   Query: {
-    getAccounts: async (): Promise<Account[]> =>
-      await prisma.account.findMany(),
-    getCategories: async (): Promise<Category[]> =>
-      await prisma.category.findMany(),
+    getAccounts: async (): Promise<Account[]> => {
+      try {
+        return await prisma.account.findMany();
+      } catch (error) {
+        throw error;
+      }
+    },
+    getCategories: async (): Promise<Category[]> => {
+      try {
+        return await prisma.category.findMany();
+      } catch (error) {
+        throw error;
+      }
+    },
     getTransactions: async (
       _: unknown,
       args: TransactionQueryInput
     ): Promise<Transaction[]> => {
-      const {
-        id,
-        first,
-        after,
-        bank,
-        account,
-        reference,
-        category,
-        amount,
-        startDate,
-        endDate,
-      } = args;
-      let where: any = {};
-      if (id) where = { ...where, id: { equals: id } };
-      if (bank)
-        where = {
-          ...where,
-          Account: { bank: { contains: bank, mode: "insensitive" } },
-        };
-      if (account)
-        where = {
-          ...where,
-          Account: {
-            ...where.Account,
-            name: { contains: account, mode: "insensitive" },
-          },
-        };
-      if (reference)
-        where = {
-          ...where,
-          reference: { contains: reference, mode: "insensitive" },
-        };
-      if (category)
-        where = {
-          ...where,
-          Category: { name: { contains: category, mode: "insensitive" } },
-        };
-      if (amount) where = { ...where, amount: { equals: amount } };
-      if (startDate) where = { ...where, date: { gte: startDate } };
-      if (endDate) where = { ...where, date: { ...where.date, lte: endDate } };
+      try {
+        const {
+          id,
+          first,
+          after,
+          bank,
+          account,
+          reference,
+          category,
+          amount,
+          startDate,
+          endDate,
+        } = args;
+        let where: any = {};
 
-      return await prisma.transaction.findMany({
-        where,
-        take: first ? first : 100,
-        skip: after ? after : 0,
-      });
+        if (id) Object.assign(where, { id: { equals: id } });
+        if (bank)
+          Object.assign(where, {
+            Account: { bank: { contains: bank, mode: "insensitive" } },
+          });
+        if (account)
+          Object.assign(where, {
+            Account: {
+              ...where.Account,
+              name: { contains: account, mode: "insensitive" },
+            },
+          });
+        if (reference)
+          Object.assign(where, {
+            reference: { contains: reference, mode: "insensitive" },
+          });
+        if (category)
+          Object.assign(where, {
+            Category: { name: { contains: category, mode: "insensitive" } },
+          });
+        if (amount) Object.assign(where, { amount: { equals: amount } });
+        if (startDate) Object.assign(where, { date: { gte: startDate } });
+        if (endDate)
+          Object.assign(where, { date: { ...where.date, lte: endDate } });
+
+        return await prisma.transaction.findMany({
+          where,
+          take: first || 100,
+          skip: after || 0,
+        });
+      } catch (error) {
+        console.error(error);
+        throw new Error("Could not fetch transactions");
+      }
     },
-    getUniqueCategories: async (): Promise<Transaction[]> =>
-      await prisma.transaction.findMany({
-        where: {},
-        distinct: ["category_id"],
-      }),
+    getUniqueCategories: async (): Promise<Transaction[]> => {
+      try {
+        return await prisma.transaction.findMany({
+          where: {},
+          distinct: ["category_id"],
+        });
+      } catch (error) {
+        throw error;
+      }
+    },
   },
   Mutation: {
     updateTransactionCategory: async (
       _: unknown,
       args: { data: TransactionInput }
     ): Promise<Transaction> => {
-      return await prisma.transaction.update({
-        where: { id: args.data.id },
-        data: { category_id: args.data.category_id },
-      });
+      try {
+        return await prisma.transaction.update({
+          where: { id: args.data.id },
+          data: { category_id: args.data.category_id },
+        });
+      } catch (error) {
+        throw error;
+      }
     },
     createCategory: async (
       _: unknown,
